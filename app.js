c0b86cbf870d6209fc2a4e56681c36a477eba54b
Marisa A Tusey <marisatusey@gmail.com>
2025-07-31 14:12:35 -0300

    actualiza la app

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto'
+let numeroSecreto = generalNumeroSecreto()
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un número del 1 al 10' ;
-
-function intentoDeUsuario() {
-    alert('Click desde el boton')
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
+console.log(numeroSecreto);
+function verificarIntento() {
+    let numeroDeUsuario = document.getElementById('valorUsuario').value;
+    console.log(numeroDeUsuario)
+    return;
+}
+
+function generalNumeroSecreto() {
+    let numeroSecreto = Math.floor(Math.random()*10)+ 1;
+    return numeroSecreto;
+}
+asignarTextoElemento('h1','juego del numero secreto!');
+asignarTextoElemento('p','Indica un número del 1 al 100');
